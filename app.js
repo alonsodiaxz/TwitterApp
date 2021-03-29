01d1444f0933c201be906917f415d805f87cc6e1
Alonso Diaz <alonsodiazsobrino@hotmail.es>
2021-03-29 18:57:27 +0200

    Arreglo de fallos

--- app.js
+++ app.js
@@ -4,27 +4,35 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var mongoose = require('mongoose');
-/*mongoose.set('UseCreateIndex', true)
-mongoose.set('UseUnifiedTopology', true)*/
-
 var indexRouter = require('./routes/index');
 var postsRouter = require('./routes/posts');
 
-var bodyParser = require('body-parser');
-var cors = require('cors');
-
-var app = express();
+var bodyParser  = require("body-parser");   //nuevo
+var cors = require('cors');   //nuevo
 
 require('dotenv').config();
+
+var mongoose = require('mongoose');
+mongoose.set('useCreateIndex', true);
+
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true })
+    .then(() =>  console.log('mymerndb connection successful'))
+    .catch((err) => console.error(err));
+
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
+
+app.use(cors());  //nuevo
+app.use(bodyParser.json({limit: '50mb'}));  //nuevo
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));   //nuevo
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -33,26 +41,18 @@ app.use('/posts', postsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    next(createError(404));
+  next(createError(404));
 });
-
-app.use(cors());
-//app.use(bodyParser().json({ limit: '50mb' }));
-//app.use(bodyParser().urlencoded({ limit: '50mb', extended: true }));
-
-mongoose.connect(process.env.DB_URI, { useNewUrlParser: true })
-    .then(() => console.log('MongoDB connection successful'))
-    .catch((err) => console.error(err));
 
 // error handler
 app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
